dbd24453c86dfd0aa991adca59c7010337b5960a
Mayank <mayankkotuli099@gmail.com>
2026-08-29 12:46:42 +0530

    updated server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,26 +1,19 @@
 import express from "express";
 import cors from "cors";
-import pricingRoutes from "./routes/pricing.js";
 
 const app = express();
 
 const PORT = 5000;
 
-// Middleware
 app.use(cors());
 app.use(express.json());
 
-// Routes
-app.use("/api/pricing", pricingRoutes);
-
-// Test route
 app.get("/", (req, res) => {
   res.json({
     message: "Intervista AI Backend is running 🚀",
   });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
